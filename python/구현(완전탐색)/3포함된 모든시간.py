2b49fbae309d0f4fdcdd59bdc21176f1a91bd89a
Jaeho <sjaeho348@gmail.com>
2026-03-13 19:05:13 +0900

    feat : 코드업 완전탐색 3이 포함된 모든시간 문제 구현

---
+++ python/구현(완전탐색)/3포함된 모든시간.py
@@ -0,0 +1,13 @@
+    
+""" 
+시,분,초를 순회하면서 카운트 증가
+"""
+n = int(input())
+count=0
+
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h)+str(m)+str(s):
+                count+=1              
+print(count)
